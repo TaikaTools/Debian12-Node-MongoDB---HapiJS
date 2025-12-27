be3a6c983b9ed34cae46e1f1e366250111b783cf
TaikaTools <support@taikatools.com>
2025-12-27 18:51:06 +0200

    Update debian12_setup.sh

--- debian12_setup.sh
+++ debian12_setup.sh
@@ -6,29 +6,35 @@
 # This script installs:
 # - Node.js 24.x LTS
 # - MongoDB 8.0 (secure, auth enabled)
-# - PM2 (process manager)
 # - Nginx (reverse proxy + fast static/uploads)
+# - Hapi.js for RestAPI, Auth, logic and database
+# - PM2 (process manager)
 # - ufw FireUncomplicated Firewall + (optional: tmux)
 # - Creates a dedicated non-root system user for the app
-# - Creates database with prompted name
 # - Generates strong random secrets .env
-# - installs HapiJS and miscellaneous
 # - Interactive SSL setup (Let's Encrypt, A+ ready)
 # - Hardening security, tweaking performance
 
 set -e  # Exit on error
 
 # 1. Prompts with defaults
-read -p "1/4 - Domain (e.g., mydomain.com, without www.): " DOMAIN
-DOMAIN=${DOMAIN:-yourdomain.com}
+read -p "1/5 - Domain (e.g., mydomain.com, without www.), blank for no domain []: " DOMAIN
+DOMAIN=${DOMAIN:-yourdomain_dot_com}
 
-read -p "2/4 - Database, Folder and User name [ntt]: " NAME
+if [ "$DOMAIN" != "yourdomain_dot_com" ]; then
+  read -p "2/5 - CertBot: fake or real (type real) []" CERT
+else
+  echo "No Domain, skipping 2/5"
+fi
+CERT=${CERT:-fake}
+
+read -p "3/5 - Database, Folder and User name [ntt]: " NAME
 NAME=${NAME:-ntt}
 
-read -p "3/4 - RestAPI (Hapi) Port [3003]: " PORT
+read -p "4/5 - RestAPI (Hapi) Port [3003]: " PORT
 PORT=${PORT:-3003}
 
-read -n 1 -r -s -p "4/4 - Install Tmux [y]: " EXTRA_TOOLS
+read -n 1 -r -s -p "5/5 - Install Tmux [y]: " EXTRA_TOOLS
 EXTRA_TOOLS=${EXTRA_TOOLS:-y}
 
 # 2. System update
@@ -65,7 +71,10 @@ if ! id "$NAME" &>/dev/null; then
 fi
 
 # 5. Tools
-sudo apt install -y ca-certificates ufw nginx certbot python3-certbot-nginx
+sudo apt install -y ufw nginx
+if [ "$DOMAIN" != "yourdomain_dot_com" ]; then
+  sudo apt install -y ca-certificates certbot python3-certbot-nginx
+fi
 if [[ $EXTRA_TOOLS == "Y" || $EXTRA_TOOLS == "y" ]]; then
   sudo apt install -y tmux
 fi
@@ -103,23 +112,32 @@ fi
 APP_DIR="/var/www/$NAME"
 sudo mkdir -p $APP_DIR
 sudo chown -R "$NAME:$NAME" $APP_DIR
+sudo chown -R www-data:www-data $APP_DIR
 sudo chmod 755 $APP_DIR
 cd $APP_DIR
 
-UPLOADS_DIR="/srv/images/$NAME"
-sudo mkdir -p $UPLOADS_DIR
-sudo chown -R "$NAME:$NAME" $UPLOADS_DIR
-sudo chmod 755 $UPLOADS_DIR
+IMAGES_DIR="/srv/images/$NAME"
+sudo mkdir -p $IMAGES_DIR
+sudo chown -R "$NAME:$NAME" $IMAGES_DIR
+sudo chown -R www-data:www-data $IMAGES_DIR
+sudo chmod 755 $IMAGES_DIR
 
 sudo mkdir -p $APP_DIR/public
+##sudo chown -R "$NAME:$NAME" $APP_DIR/public
 sudo chown www-data:www-data $APP_DIR/public
 sudo chmod 755 $APP_DIR/public
 
+sudo mkdir -p $APP_DIR/logs
+##sudo chown -R "$NAME:$NAME" $APP_DIRlogs
+sudo chown www-data:www-data $APP_DIR/logs
+sudo chmod 755 $APP_DIR/logs
+
 # 9. Save secrets to .env (only if not already present)
 cat > .env <<EOF
+DOMAIN=$DOMAIN
 PORT=$PORT
-DOMAIN=https://$DOMAIN
-UPLOADS_PATH=$UPLOADS_DIR
+UPLOADS_PATH=$IMAGES_DIR
+NAMEUSERFOLDER=$NAME
 MONGODB_APP="mongodb://app:$APP_PASS@127.0.0.1:27017/$NAME?authSource=$NAME"
 # MONGODB_ADMIN="mongodb://admin:$ADMIN_PASS@127.0.0.1:27017/$NAME?authSource=$NAME"
 JWT_SECRET=$JWT_SECRET
@@ -139,39 +157,70 @@ npm install @hapi/hapi @hapi/boom @hapi/joi @hapi/jwt @hapi/cookie @hapi/inert m
 
 # 10. Nginx config
 sudo bash -c "cat > /etc/nginx/sites-available/$NAME <<'EOF'
+
 server {
     listen 80;
-    server_name _;
+    listen [::]:80;
 
-    location /uploads/ {
-        alias $UPLOADS_DIR/;
-        expires 30d;
-        add_header Cache-Control \"public\";
+    #!#!# server_name $DOMAIN www.$DOMAIN;
+
+    location /images/ {
+        alias $IMAGES_DIR/;
+        expires 27d;
+        add_header Cache-Control "public";
         access_log off;
     }
 
+    return 301 https://\$host\$request_uri;
+}
+
+server {
+    listen 443;
+    listen [::]:443;
+
+    #!#!# server_name $DOMAIN www.$DOMAIN;
+
+    # ssl_certificate /etc/ssl/private/selfsigned.crt;
+    # ssl_certificate_key /etc/ssl/private/selfsigned.key;
+
+    #%#%# ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;
+    #%#%# ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;
+    #%#%# include /etc/letsencrypt/options-ssl-nginx.conf;
+    #%#%# ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+
     location /api/ {
+        rewrite ^/api/(.*) /\$1 break;
         proxy_pass http://127.0.0.1:$PORT;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection \"upgrade\";
+        # proxy_set_header Upgrade \$http_upgrade;
+        # proxy_set_header Connection \"upgrade\";
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
+        # proxy_set_header X-Forwarded-Proto \$scheme;
+
+        proxy_pass_header Set-Cookie;
+        proxy_cookie_path / /api/;
+        proxy_buffering off;
+        
+        # proxy_cookie_domain localhost \$host; #Optional: If backend sets a domain, rewrite it (rare for localhost)
+        # proxy_redirect off;
+        # proxy_set_header X-Forwarded-For \$remote_addr;
+        # proxy_set_header X-Forwarded-Proto https;
+        # proxy_intercept_errors on;
+        # add_header X-Cache-Status \$upstream_cache_status;        
+        # proxy_ssl_verify off; #Required for SSL passthrough
     }
 
     root /var/www/$NAME/public;
 
     location / {
-        ## try_files \$uri \$uri/ =404;
         try_files \$uri \$uri/ /index.html;
         ## expires 1h;
         ## add_header Cache-Control "public";
     }
 
-    ## location ~ /\.env { deny all; }
-    ## location ~ /\.git { deny all; }
+    location ~ /\.env { deny all; }
 }
 EOF"
 
@@ -198,22 +247,27 @@ sudo ufw deny out on wlan0
 sudo ufw enable
 
 # 12. SSL
-if [ "$DOMAIN" != "yourdomain.com" ]; then
-  sudo sed -i "s/server_name _;/server_name $DOMAIN www.$DOMAIN;/" /etc/nginx/sites-available/$NAME
+if [ "$DOMAIN" != "yourdomain_dot_com" ]; then
+  sudo sed -i "s/#!#!# server_name/server_name/" /etc/nginx/sites-available/$NAME
   sudo nginx -t && sudo systemctl reload nginx
-  sudo certbot --nginx -d "$DOMAIN" -d "www.$DOMAIN"
-  # A+ snippet...
-  sudo bash -c 'cat > /etc/nginx/snippets/ssl-params.conf <<EOF
-ssl_protocols TLSv1.2 TLSv1.3;
-ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305;
-ssl_session_timeout 1d;
-ssl_session_cache shared:SSL:10M;
-ssl_session_tickets off;
-add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
-EOF'
-  sudo sed -i '/listen 443/a include /etc/nginx/snippets/ssl-params.conf;' /etc/nginx/sites-available/$NAME
-  sudo openssl dhparam -dsaparam -out /etc/nginx/dhparam.pem 4096
-  echo "ssl_dhparam /etc/nginx/dhparam.pem;" | sudo tee -a /etc/nginx/snippets/ssl-params.conf
+  if [ "$CERT" != "fake" ]; then
+    #sudo certbot --nginx -d "$DOMAIN" -d "www.$DOMAIN"
+    # A+ snippet...
+    sudo bash -c 'cat > /etc/nginx/snippets/ssl-params.conf <<EOF
+  ssl_protocols TLSv1.2 TLSv1.3;
+  ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305;
+  ssl_session_timeout 1d;
+  ssl_session_cache shared:SSL:10M;
+  ssl_session_tickets off;
+  add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
+  EOF'
+    sudo sed -i '/listen 443/a include /etc/nginx/snippets/ssl-params.conf;' /etc/nginx/sites-available/$NAME
+    sudo openssl dhparam -dsaparam -out /etc/nginx/dhparam.pem 4096
+    echo "ssl_dhparam /etc/nginx/dhparam.pem;" | sudo tee -a /etc/nginx/snippets/ssl-params.conf
+  else
+    echo "step to make";
+    #sudo certbot --nginx -d "$DOMAIN" -d "www.$DOMAIN"
+  fi
   sudo nginx -t && sudo systemctl reload nginx
 fi
 
@@ -230,6 +284,7 @@ use $NAME
 db.createUser({ user: "app", pwd: "$APP_PASS", roles: [ "readWrite" ] })
 exit
 EOF
+fi
 
 # 15. Final
 echo "=============================================================="
@@ -238,7 +293,7 @@ echo "Admin: $ADMIN_PASS"
 echo "App: $APP_PASS"
 echo "JWT: $JWT_SECRET"
 
-if [ "$DOMAIN" != "yourdomain.com" ]; then
+if [ "$DOMAIN" != "yourdomain_dot_com" ]; then
 echo "ssl_certificate /etc/letsencrypt/live/$DOMAIN/fullchain.pem;"
 echo "ssl_certificate_key /etc/letsencrypt/live/$DOMAIN/privkey.pem;"
 fi
